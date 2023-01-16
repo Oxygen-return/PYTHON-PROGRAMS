3d3f11763731335aef0352cff22cdec1e5db0eaa
unknown <matthewinyumili@gmail>
2023-01-16 15:45:07 +0300

    first commit

---
+++ myname.py
@@ -0,0 +1,19 @@
+#my first python program
+print('Isaac Mutuma!')
+
+
+#interger
+b=20
+print(b)
+
+#string
+name= "Elon musk"
+print(name)
+
+phone_number= "0720000000"
+print(phone_number)
+
+#adding two intergers
+a=10
+b=20
+print(a+b)
